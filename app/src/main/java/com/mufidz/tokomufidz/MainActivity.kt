21a9906052629e0208b36409d5c6245c9aaebfd9
Mufidz027 <almusyafir240@gmail.com>
2020-12-14 00:10:41 +0700

    Initial commit

---
+++ app/src/main/java/com/mufidz/tokomufidz/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mufidz.tokomufidz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.belajar_linier_layout)
+            setContentView(R.layout.belajar2_linier)
+        setContentView(R.layout.belajar_constraint_layout)
+        }
+    }
